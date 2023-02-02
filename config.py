4056750fbb4bb35ea12cad25023491d3aa3e8be3
kkadambi@microsoft.com <kkadambi@microsoft.com>
2023-02-02 23:51:19 +0530

    PTFCA Files

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+from os import environ 
+
+class Config(object):
+    DEBUG = False
+    TESTING = False 
+    
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    
+    SECRET_Key = "koushilabs"
+    DB_NAME = "production-db"
+    DB_USERNAME = "root"
+    DB_PASSWORD = "koushilabs"
+    
+    UPLOADS = "/home/username/app/app/statis/uploads"
+    
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+    
+    
+class ProductionConfig(Config):
+    pass 
+
+class DevelopmentConfig(Config):
+    DEBUG = True 
+    
+    DB_NAME = "production-db"
+    DB_USERNAME = "root"
+    DB_PASSWORD = "koushilabs"
+    
+    UPLOADS = "/home/username/app/app/static/uploads"
+    SESSION_COOKIE_SECURE = False 
+    
+class DebugConfig(Config):
+    DEBUG = False 
+    
